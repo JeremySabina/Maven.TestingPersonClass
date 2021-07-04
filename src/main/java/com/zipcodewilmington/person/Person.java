ca1c828db385f7dc0338dd5ae501838dd38ddc04
Jeremy <jeremysabina@gmail.com>
2021-07-04 12:00:51 -0400

    Added 5 additional features of a person

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,6 +6,11 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private String eyeColor;
+    private int amountOfNoseHairs;
+    private int lengthOfToes;
+    private String colorOfShirt;
+    private String colorOfHair;
 
     public Person() {
         this.name = "";
@@ -25,6 +30,38 @@ public class Person {
         this.age = age;
     }
 
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public void setAmountOfNoseHairs(int amountOfNoseHairs) {
+        this.amountOfNoseHairs = amountOfNoseHairs;
+    }
+
+    public void setLengthOfToes(int lengthOfToes) {
+        this.lengthOfToes = lengthOfToes;
+    }
+
+    public void setColorOfShirt(String colorOfShirt) {
+        this.colorOfShirt = colorOfShirt;
+    }
+
+    public void setColorOfHair(String colorOfHair) {
+        this.colorOfHair = colorOfHair;
+    }
+
+    public Person(String name, int age, String eyeColor, Integer amountOfNoseHairs, String colorOfHair,
+                  String colorOfShirt, int lengthOfToes) {
+
+        this.name = name;
+        this.age = age;
+        this.colorOfShirt = colorOfShirt;
+        this.amountOfNoseHairs = amountOfNoseHairs;
+        this.eyeColor = eyeColor;
+        this.colorOfHair = colorOfHair;
+        this.lengthOfToes = lengthOfToes;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -41,4 +78,26 @@ public class Person {
     public Integer getAge() {
         return age;
     }
+
+    public String getEyeColor() { return eyeColor; }
+
+    public Integer getAmountOfNoseHairs() { return amountOfNoseHairs; }
+
+    public Integer getLengthOfToes() { return lengthOfToes; }
+
+    public Person(String name, int age, String eyeColor, int amountOfNoseHairs, int lengthOfToes, String colorOfShirt, String colorOfHair) {
+        this.name = name;
+        this.age = age;
+        this.eyeColor = eyeColor;
+        this.amountOfNoseHairs = amountOfNoseHairs;
+        this.lengthOfToes = lengthOfToes;
+        this.colorOfShirt = colorOfShirt;
+        this.colorOfHair = colorOfHair;
+    }
+
+    public String getColorOfHair() {
+        return colorOfHair;
+    }
+
+    public String getColorOfShirt() { return colorOfShirt; }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -95,4 +95,73 @@ public class TestPerson {
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void testConstructorWithEyeColor() {
+        // Given
+        Person person = new Person();
+        String expected = "Blue";
+
+        // When
+        person.setEyeColor(expected);
+
+        // Then
+        String actual = person.getEyeColor();
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testConstructorWithColorOfHair() {
+        // Given
+        Person person = new Person();
+        String expected = "Blonde";
+
+        // When
+        person.setColorOfHair(expected);
+        // Then
+        String actual = person.getColorOfHair();
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testConstructorWithColorOfShirt() {
+        // Given
+        Person person = new Person();
+        String expected = "Purple";
+
+        // When
+        person.setColorOfShirt(expected);
+        // Then
+        String actual = person.getColorOfShirt();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConstructorWithAmountOfNoseHairs() {
+        // Given
+        Person person = new Person();
+        Integer expected = 100;
+
+        // When
+        person.setAmountOfNoseHairs(expected);
+        // Then
+        Integer actual = person.getAmountOfNoseHairs();
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testConstructorWithLengthOfToes() {
+        // Given
+        Person person = new Person();
+        Integer expected = 7;
+
+        // When
+        person.setLengthOfToes(expected);
+        // Then
+        Integer actual = person.getLengthOfToes();
+        Assert.assertEquals(expected, actual);
+    }
 }
